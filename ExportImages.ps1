f4e9e32af604cea5c6428a65c5c8586122a7d8fa
Stefan Büchler <36002004+stefbuechler@users.noreply.github.com>
2020-11-29 19:43:01 +0100

    Add files via upload

---
+++ ExportImages.ps1
@@ -0,0 +1,31 @@
+﻿$SourcePath = "C:\Temp\Input"
+$DestinationPath = "C:\Temp\Export"
+New-Item -Path $DestinationPath -ItemType Directory
+
+$ItemTypes = '*.jpg','*.png','*.gif'
+
+$SecuenceNumber = 0
+
+foreach ($item in $ItemTypes){
+
+$Files = Get-ChildItem -Path $SourcePath -Recurse -Include $item 
+
+foreach ($file in $Files){
+
+Get-ChildItem -Path $File | Rename-Item -NewName {$_.Basename.Replace("image","$SecuenceNumber") + $_.extension}
+
+
+$SecuenceNumber ++
+}
+}
+
+
+foreach ($item in $ItemTypes){
+
+$Files = Get-ChildItem -Path $SourcePath -Recurse -Include $item 
+
+foreach ($file in $Files){
+Move-Item -Path $file -Destination $DestinationPath
+
+
+}}
